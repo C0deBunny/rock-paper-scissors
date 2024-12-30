f1b2abf50b21d9f04b17e8a7eb6017292c16102d
Usagi <118476704+C0deBunny@users.noreply.github.com>
2024-12-30 22:26:05 +0100

    Get computer choice for rock, paper, scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(`Hello World!`)
+function getComputerChoice() {
+    let randomNumber = Math.random()
+
+    if (randomNumber <= 0.33) {
+        return `Rock`
+    } else if (randomNumber <= 0.66) {
+        return `Paper`
+    } else {
+        return `Scissors`
+    }
+}
+
+console.log(getComputerChoice())
