7084a212d1770ddc78b63e315f398d95f27ae084
Usagi <118476704+C0deBunny@users.noreply.github.com>
2025-01-01 16:35:58 +0100

    Add playRoundX() for best out of x

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function playRound() {
+// Scoreboard
+let humanScore = 0
+let computerScore = 0
 
-    // Reset choices
-    let humanChoice = ``
-    let computerChoice = ``
+// Play one Round
+function playRound() {
 
     // Gets players choice
     function getHumanChoice() {
@@ -43,23 +44,62 @@ function playRound() {
 
     // Determine the winner
     if (humanChoice === computerChoice) {
-        return `You Draw! both players chose ` + humanChoice
+        console.log(`You Draw! both players chose ` + humanChoice)
+        console.log(``)
+        console.log(`Lets try again..`)
+        return playRound()
     } else if (humanChoice === `rock` && computerChoice === `scissors`) {
+        humanScore++
         return `You Win! Rock smashes scissors`
     } else if (humanChoice === `paper` && computerChoice === `rock`) {
+        humanScore++
         return `You Win! Paper wraps rock`
     } else if (humanChoice === `scissors` && computerChoice === `paper`) {
+        humanScore++
         return `You Win! Scissors cuts paper`
     } else if (humanChoice === `scissors` && computerChoice === `rock`) {
+        computerScore++
         return `You Lose! Rock smashes scissors`
     } else if (humanChoice === `rock` && computerChoice === `paper`) {
+        computerScore++
         return `You Lose! Paper wraps rock`
     } else if (humanChoice === `paper` && computerChoice === `scissors`) {
+        computerScore++
         return `You Lose! Scissors cuts paper`
     } else {
         return `playRound() has bug? xD`
     }
 }
 
-// Play round
-// console.log(playRound())
+// Play X amount of rounds
+function playRoundX() {
+
+    // Reset score
+    humanScore = 0
+    computerScore = 0
+
+    roundNumber = (prompt(`How many rounds do you wish to play?`))
+    while (!parseInt(roundNumber)) {
+        alert(roundNumber + ` is not a number..  :c`)
+        roundNumber = (prompt(`How many rounds do you wish to play?`))
+    }
+    // console.log(roundNumber)
+
+    for (i = 0; i < roundNumber; i++) {
+        console.log(`Start round: ` + (i + 1))
+
+        console.log(playRound())
+        console.log(``)
+        console.log(`Scoreboard:`)
+        console.log(`Player score: ` + humanScore + ` | Computer score: ` + computerScore)
+        console.log(``)
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You won the battle! :D`)
+    } else if (humanScore < computerScore) {
+        console.log(`HAHA COMPUTER WON! YOU SUCK! XD`)
+    } else {
+        console.log(`Looks like it's a draw.. well played!`)
+    }
+}
